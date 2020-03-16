6bba5b9fa80c177cf62c7f9cbf1fb45a77fe5bd4
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2020-03-16 16:34:39 +0100

    Update logs_cleaner1.sh

--- logs_cleaner1.sh
+++ logs_cleaner1.sh
@@ -1,19 +1,19 @@
 # put the file in /usr/local/scripts (or whre you want) and define a crontab like as:
 # 0 1 * * * /usr/local/scripts/logs_cleaner.sh /opt/tomcat/logs > /dev/null 2>&1
 
-if [ -d "$ARG1" ]
+if [ -d "$1" ]
 then
-      echo "\$ARG1 exists"
+      echo "\$1 exists"
 
 # if you want to override ARG1 then
 # ARG1=/opt/tomcat/logs
 
 # remove the old, already compressed logs
-find $ARG1 -maxdepth 1 -type f -mtime +370 | while read filename ; do fuser -s $filename || rm -f $filename ; done
+find $1 -maxdepth 1 -type f -mtime +370 | while read filename ; do fuser -s $filename || rm -f $filename ; done
 
 # compress to xz
-find $ARG1 -type f | while read filename ; do fuser -s $filename || xz -9fz $filename ; done
+find $1 -type f | while read filename ; do fuser -s $filename || xz -9fz $filename ; done
 
 else
-      echo "\$ARG1 should be an existing path"
+      echo "\$1 should be an existing path"
 fi
