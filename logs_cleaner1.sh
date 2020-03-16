40a5813edfe506bc0dd1bd6b55eb4b4dad327fbb
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2020-03-16 16:29:39 +0100

    Update logs_cleaner1.sh

--- logs_cleaner1.sh
+++ logs_cleaner1.sh
@@ -3,7 +3,7 @@
 
 if [ -d "$ARG1" ]
 then
-      echo "$ARG1 exists"
+      echo "\$ARG1 exists"
 
 # if you want to override ARG1 then
 # ARG1=/opt/tomcat/logs
@@ -15,5 +15,5 @@ find $ARG1 -maxdepth 1 -type f -mtime +370 | while read filename ; do fuser -s $
 find $ARG1 -type f | while read filename ; do fuser -s $filename || xz -9fz $filename ; done
 
 else
-      echo "$ARG1 should be an existing path"
+      echo "\$ARG1 should be an existing path"
 fi
