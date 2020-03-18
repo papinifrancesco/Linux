dac2a1d8767097aaeb3404a2cf1631e87ec715e3
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2020-03-18 10:56:35 +0100

    Update logs_cleaner.sh
    $2 is the number of days for mtime

--- logs_cleaner.sh
+++ logs_cleaner.sh
@@ -1,12 +1,13 @@
 # put the file in /usr/local/scripts (or whre you want) and define a crontab like as:
-# 0 1 * * * /usr/local/scripts/logs_cleaner.sh /opt/tomcat/logs > /dev/null 2>&1
+#                                                  FOLDER        DAYS
+# 0 1 * * * /usr/local/scripts/logs_cleaner.sh /opt/tomcat/logs/ 370 > /dev/null 2>&1
 
 # if the path passed as an argument exists
 if [ -d "$1" ]
 
 then
 # remove the old files but always check first that are not opened by a process
-find $1 -maxdepth 1 -type f -mtime +370 | while read filename ; do fuser -s $filename || rm -f $filename ; done
+find $1 -maxdepth 1 -type f -mtime +$2 | while read filename ; do fuser -s $filename || rm -f $filename ; done
 
 # compress to xz only a file what is not a xz archive already
 # xz is smart enough to do that check itself but we'd waste time
