252392c73749f89b9b39be97cbdc99888ca6e27e
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2020-05-26 15:59:24 +0200

    Update logs_recompress.sh

--- logs_recompress.sh
+++ logs_recompress.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # if the path passed as an argument exists
 if [ -d "$1" ]
+
 then
 cd $1
+
+# remove the 0 bytes files but always check first that are not opened by a process
+find $1 -maxdepth 1 -type f -size 0 | while read filename ; do /sbin/fuser -s $filename || rm -f $filename ; done
+
 for filename in ./*.gz; do
     gzip -d $filename
     decompressed=${filename%.gz}
