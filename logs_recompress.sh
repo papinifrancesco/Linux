4f94a1f5dd1e4f272c1afc5124f7900f9cdc3f17
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2020-05-26 15:29:11 +0200

    Update logs_recompress.sh

--- logs_recompress.sh
+++ logs_recompress.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# if the path passed as an argument exists
+if [ -d "$1" ]
+then
 cd $1
 for filename in ./*.gz; do
     gzip -d $filename
     decompressed=${filename%.gz}
     xz -9fz $decompressed
 done
+cd -
