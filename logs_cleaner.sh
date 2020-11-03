3a427690f05425c47b5973affebfcbdceef68cc9
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2020-11-03 17:10:48 +0100

    Update logs_cleaner.sh

--- logs_cleaner.sh
+++ logs_cleaner.sh
@@ -1,7 +1,7 @@
-# 202011031700
+# 202011031710
 # put the file in /usr/local/scripts (or whre you want) and define a crontab like as:
-#                                                  FOLDER        DAYS
-# 0 1 * * * /usr/local/scripts/logs_cleaner.sh /opt/tomcat/logs/ 370 > /dev/null 2>&1
+#                                                  FOLDER        DAYS  ARCHIVE FOLDER
+# 0 1 * * * /usr/local/scripts/logs_cleaner.sh /opt/tomcat/logs/ 370   history        > /dev/null 2>&1
 
 # if the path passed as an argument exists
 if [ -d "$1" ]
@@ -16,9 +16,16 @@ find $1 -maxdepth 1 -type f -mtime +$2 | while read filename ; do /sbin/fuser -s
 # xz is smart enough to do that check itself but we'd waste time
 find $1 -maxdepth 1 -type f \( ! -name "*.?z" \) | while read filename ; do /sbin/fuser -s $filename || xz -9fz $filename ; done
 
-mv $1/*.xz $1/history/
-
 # if the path passed as an argument does not exist
 else
       echo "\$1 should be an existing path"
+fi
+
+if [ -d "$1/$3" ]
+
+then
+mv $1/*.xz $1/history/
+
+else
+      echo "\$1/\$3 should be an existing path"
 fi
