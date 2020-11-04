67eb064bd4fc37a544c904054a5e8028fac375f3
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2020-11-04 12:14:08 +0100

    Update logs_cleaner.sh

--- logs_cleaner.sh
+++ logs_cleaner.sh
@@ -1,4 +1,4 @@
-# 202011031752
+# 202011041126
 # put the file in /usr/local/scripts (or whre you want) and define a crontab like as:
 #                                                  FOLDER        DAYS  ARCHIVE FOLDER
 # 0 1 * * * /usr/local/scripts/logs_cleaner.sh /opt/tomcat/logs  370   history        > /dev/null 2>&1
@@ -12,18 +12,22 @@ find $1 -maxdepth 1 -type f -size 0 | while read filename ; do /sbin/fuser -s $f
 # remove the old files but always check first that are not opened by a process
 find $1 -maxdepth 1 -type f -mtime +$2 | while read filename ; do /sbin/fuser -s $filename || rm -f $filename ; done
 
-# compress to xz only a file what is not a xz archive already
-# xz is smart enough to do that check itself but we'd waste time
-find $1 -maxdepth 1 -type f \( ! -name "*.?z" \) | while read filename ; do /sbin/fuser -s $filename || xz -9fz $filename ; done
-
 # if the path passed as an argument does not exist
 else
       echo "\$1 should be an existing path"
 fi
 
+
 if [ ! -z $3 ] && [ -d "$1/$3" ]
 
 then
+
+# compress to xz only a file what is not a xz archive already
+# xz is smart enough to do that check itself but we'd waste time
+find ./ -maxdepth 1 \( -type f ! -name "*.?z" \)          -a \
+                    \( -type f ! -name "catalina.out*" \)    \
+                    | while read filename ; do /sbin/fuser -s $filename || xz -9fz $filename ; done
+
 mv $1/*.xz $1/$3/
 
 else
