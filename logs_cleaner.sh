21204d11d41c764546c7f3532327114e9407882d
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2020-11-18 11:17:06 +0100

    Update logs_cleaner.sh

--- logs_cleaner.sh
+++ logs_cleaner.sh
@@ -1,4 +1,4 @@
-# 202011091753
+# 202011181117
 # put the file in /usr/local/scripts (or whre you want) and define a crontab like as:
 #                                                  FOLDER        DAYS  ARCHIVE FOLDER
 # 0 1 * * * /usr/local/scripts/logs_cleaner.sh /opt/tomcat/logs  370   history        > /dev/null 2>&1
@@ -9,8 +9,6 @@ if [ -d "$1" ]
 then
 # remove the 0 bytes files but always check first that are not opened by a process
 find $1 -maxdepth 1 -type f -size 0 | while read filename ; do /sbin/fuser -s $filename || rm -f $filename ; done
-# remove the old files but always check first that are not opened by a process
-find $1 -maxdepth 1 -type f -mtime +$2 | while read filename ; do /sbin/fuser -s $filename || rm -f $filename ; done
 
 # if the path passed as an argument does not exist
 else
@@ -29,6 +27,10 @@ find $1 -maxdepth 1 \( -type f ! -name "*.?z" \)          -a \
                     | while read filename ; do /sbin/fuser -s $filename || xz -9fz $filename ; done
 
 mv $1/*.xz $1/$3/
+
+# remove the old files but always check first that are not opened by a process
+find $1/$3/ -maxdepth 1 -type f -mtime +$2 | while read filename ; do /sbin/fuser -s $filename || rm -f $filename ; done
+
 
 else
       echo "archive folder unset or non existing"
