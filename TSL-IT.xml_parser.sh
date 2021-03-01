98db7daaa9d427d80823e7278fc2a55628e1d7e2
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2021-03-01 09:20:08 +0100

    Create TSL-IT.xml_parser.sh

---
+++ TSL-IT.xml_parser.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# https://eidas.agid.gov.it/TL/TSL-IT.xml
+# goal: to have list of not expired CAs , ready to be used by httpd,
+# with comments before each certificate, example:
+
+# certificate CN
+# -----BEGIN CERTIFICATE-----
+# [data]
+# -----END CERTIFICATE-----BEGIN
