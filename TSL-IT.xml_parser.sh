37d6a4f6535cb000b1636594b34d81f4d3ad8b84
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2021-03-18 17:12:49 +0100

    Update TSL-IT.xml_parser.sh
    Looks for IdV CAs and reports the ones to be dismissed because expired and never renewed.

--- TSL-IT.xml_parser.sh
+++ TSL-IT.xml_parser.sh
@@ -1,71 +1,101 @@
 #!/bin/bash
+# use a dedicated folder
 # usage: ./TSL-IT.xml_parser.sh > /dev/null 2>&1
 
-# thanks to: https://github.com/eniocarboni/getTrustCAP7m
-# for inspiring me
+# thanks to: https://github.com/eniocarboni/getTrustCAP7m for inspiring me
 
 # goal: refine TSL-IT.xml to have list of not expired CAs ,
 # ready to be used by httpd, with identifying distinguished name
-# before each certificate, example:
-
-# certificate subject
-# -----BEGIN CERTIFICATE-----
-# [data]
-# -----END CERTIFICATE-----BEGIN
+# before each certificate
 
 
 XML_CERTS='https://eidas.agid.gov.it/TL/TSL-IT.xml'
-CA_XML='CA.xml'
 CA_PEM='CA.pem'
 
-wget --tries=2 -O CA.xml ${XML_CERTS}
-# TSL-IT.xml now is only one line long so we add a return value before start tag and after tag (X509Certificate)
-for i in $(sed -e 's/<X509Certificate/\n<X509Certificate/g' -e s'#</X509Certificate>#</X509Certificate>\n#g' "$CA_XML" | grep '<X509Certificate'); do
-  echo -e "-----BEGIN CERTIFICATE-----"o
-  echo "$i"| sed -e 's/<\/*X509Certificate>//g'| openssl base64 -d -A| openssl base64
-  echo -e "-----END CERTIFICATE-----"
-done > "$CA_PEM"
-rm -f "$CA.xml"
+# get the eIDAS certificates list
+wget --tries=2 -O CA_ALL.xml ${XML_CERTS} 
+
+
+# lets remove the whole <ServiceHistory> sections
+# it leads to false "IdV" later on
+sed '/<ServiceHistory>/,/<\/ServiceHistory>/d' CA_ALL.xml > CA_tmp1.xml
+
+
+# We want only "IdV" CAs
+awk '/IdV<\/ServiceTypeIdentifier>/,/<\/X509Certificate>/' CA_tmp1.xml > CA_tmp2.xml
+
+
+# We just want the certificate name exactly as reported by eIDAS
+# and the certificate itself
+grep -E '<Name |X509Certificate' CA_tmp2.xml > CA_tmp3.xml 
+
+
+# clean the gertificate name and leave it as a comment (used later)
+sed -e 's/.*<Name xml:lang="en">/# /g' -e 's/<\/Name>//g' CA_tmp3.xml > CA_tmp4.xml
+
+
+# lets define BEGIN and END CERTIFICATE
+sed -e 's/<X509Certificate>/-----BEGIN CERTIFICATE-----\n/g' -e 's#</X509Certificate>#\n-----END CERTIFICATE-----\n#g' CA_tmp4.xml > CA.pem
 
 
 # Divide $CA_PEM into single certificate files
-csplit -s -z -f cert- "$CA_PEM" '/-----BEGIN CERTIFICATE-----/' '{*}'
+csplit -s -z -f cert- "$CA_PEM" "/# CN/" '{*}'
 
 
-# check that each certificate won't expire in one second
-# another way to say: check that the certificate isn't expire already
-for filename in ./cert-* ; do openssl x509 -checkend 1 -in "$filename"
+# check that the certificate hasn't expired already
+# echo "These certificate have expired:"
+for filename in ./cert-* ; do openssl x509 -checkend 0 -in "$filename" > /dev/null 2>&1 ; ExitCode=$?
 
-# if expired, delete it
-if [ $? -eq 1 ]; then
-  rm -f "$filename"
-fi
-
-done
-
-# check that each certificate has 1.3.159.6.5.1 policy
-for filename in ./cert-* ; do openssl x509 -noout -text -ext certificatePolicies -in "$filename" | grep -m1 -F '1.3.159.6.5.1'
-
-if [ $? -eq 1 ]; then
+# if expired, show which one it is and then delete it
+if [ "$ExitCode" -eq 1 ]; then
+  head -n1 "$filename" >> expired_CA_preliminary_list.txt
   rm -f "$filename"
 else
 
-# canonicalize the file and prepend its subject
-openssl x509 -subject -in "$filename" -out "$filename.pem"
+# lets save our subject as eIDAS wrote it
+comment=$(head -n1 "$filename")
 
-fi
+# canonicalize the file and prepend its subject
+openssl x509 -in "$filename" -out "$filename.pem"
+
+# add the comment to the cert
+echo "$comment"|cat - "$filename.pem" > /tmp/out && mv /tmp/out "$filename.pem"
+
+# add a newline to the cert
+echo "" >> "$filename.pem"
 
 # remove the old temporary file
 rm -f "$filename"
 
+fi
 done
+
 
 # concatenate all the single file certificates into a one, single, big, file.
 cat cert-*.pem > ca_abilitate.pem
 
-# substitute "subject" with a newline a # and a space
-# I like to clearly delimit one certificate from the next one
-sed -i 's/subject=/\n# /g' ca_abilitate.pem
+
+# extract just a list of the actual, good, IdV, CAs
+grep \# ca_abilitate.pem > actual_CA_list.txt
+
+
+# do we really need to sort the files?
+# not really but I liked it
+sort expired_CA_preliminary_list.txt > expired_CA_preliminary_list_sorted.txt
+sort actual_CA_list.txt > actual_CA_list_sorted.txt
+
+
+# check if the expired CAs have been replaced with new certificates:
+# I want to see only the ones that have expired and never renewed
+echo "Expired, not renewed CAs"
+
+while IFS= read -r line; do
+    grep -q "$line" actual_CA_list_sorted.txt ;
+if [ $? -eq 1 ]; then
+  echo "$line"
+fi
+done < expired_CA_preliminary_list_sorted.txt ; 
+
 
 # do the cleanings
-rm -f cert-*.pem CA.pem CA.xml
+rm -f cert-* CA* expired_CA_* actual_CA_*
