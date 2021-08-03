a374e361a9a0e69db3cadd419d0273e12d1b280a
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2021-08-03 12:21:45 +0200

    Create diffa.sh

---
+++ diffa.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# usage:
+# diffa.sh path1/to1/file1 path2/to2/file2
+# doesn't work for XML files
+# the logic here is to compare files that have an ATTRIBUTE=VALUE structure
+
+# remove the lines beginning with "#" because are just comments
+sed '/^#/ d' < "$1" > /tmp/temp1.txt
+sed '/^#/ d' < "$2" > /tmp/temp2.txt
+
+
+# sort and remove duplicates
+sort /tmp/temp1.txt | uniq > /tmp/temp3.txt
+sort /tmp/temp2.txt | uniq > /tmp/temp4.txt
+
+# compare
+diff -y -W220 /tmp/temp3.txt /tmp/temp4.txt
