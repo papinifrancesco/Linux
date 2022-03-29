e7f2762e104ca508df8da555afd90b5133a47db0
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2022-03-29 12:19:24 +0200

    Update diffa.sh

--- diffa.sh
+++ diffa.sh
@@ -11,8 +11,8 @@ sed '/^#/ d' < "$2" > /tmp/temp2.txt
 
 
 # sort duplicates , investigate the file in case of duplicates!!
-sort /tmp/temp1.txt | uniq > /tmp/temp3.txt
-sort /tmp/temp2.txt | uniq > /tmp/temp4.txt
+sort /tmp/temp1.txt > /tmp/temp3.txt
+sort /tmp/temp2.txt > /tmp/temp4.txt
 
 # compare
 diff -y -W220 /tmp/temp3.txt /tmp/temp4.txt
